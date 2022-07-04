7af70b455e368eb0ae30c049eee0c86f5bbc3927
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-07-04 16:01:28 +0900

    Feat: 2004_CombinationNumOfZero 성공
    2004_CombinationNumOfZero
    성공
    오답노트 필요

--- 2004_CombinationNumOfZero/2004_CombinationNumOfZero/CombinationNumOfZero.cpp
+++ 2004_CombinationNumOfZero/2004_CombinationNumOfZero/CombinationNumOfZero.cpp
@@ -1,54 +1,43 @@
 #include <iostream>
+using namespace std;
+using L = unsigned long long;
 
-int ComCntDiv(int N, int M, int div)
+L ComCntDiv(L N, L M, L div)
 {
-	int cnt = 0;
+	L cnt = 0;
 
-	if (N < div || N <= M)
+	for (L i = div; N / i >= 1; i *= div)
 	{
-		return 0;
+		cnt += N / i;
 	}
-	else
+
+	for (L i = div; M / i >= 1; i *= div)
 	{
-		int temp = N;
-		if (temp % div == 0)
-		{
-			while (temp % div == 0)
-			{
-				cnt++;
-				temp /= div;
-			}
-		}
-
-		temp = N - M;
-		if (temp % div == 0)
-		{
-			while (temp % div == 0)
-			{
-				cnt--;
-				temp /= div;
-			}
-		}
-
-		cnt += ComCntDiv(N - 1, M, div);
-		return cnt;
+		cnt -= M / i;
 	}
+
+	for (L i = div; (N - M) / i >= 1; i *= div)
+	{
+		cnt -= (N - M) / i;
+	}
+
+	return cnt;
 }
 
-int N, M;
+L N, M;
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-	std::cin >> N >> M;
+	cin >> N >> M;
 
-	int twoCnt = ComCntDiv(N, M, 2);
-	int fiveCnt = ComCntDiv(N, M, 5);
+	L twoCnt = ComCntDiv(N, M, 2);
+	L fiveCnt = ComCntDiv(N, M, 5);
 
-	int result = (twoCnt > fiveCnt) ? fiveCnt : twoCnt;
-	std::cout << result;
+	L result = (twoCnt > fiveCnt) ? fiveCnt : twoCnt;
+	cout << result;
 
 	return 0;
 }
