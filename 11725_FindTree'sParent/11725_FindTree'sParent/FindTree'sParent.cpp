4598f4e72bcdca5752b02c940e7ee98132d86a1a
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-07-04 13:47:01 +0900

    Feat: 11725_FindTree'sParent 성공
    11725_FindTree'sParent
    성공

--- 11725_FindTree'sParent/11725_FindTree'sParent/FindTree'sParent.cpp
+++ 11725_FindTree'sParent/11725_FindTree'sParent/FindTree'sParent.cpp
@@ -2,9 +2,22 @@
 #include <deque>
 using namespace std;
 
-deque<int> tree[100'000];
-int parent[100'000] = { 0 };
+deque<int> tree[100'001];
+int parent[100'001] = { 0 };
 int N;
+
+void dfs(int ver)
+{
+	for (int next : tree[ver])
+	{
+		if (parent[next] == 0)
+		{
+			parent[next] = ver;
+			dfs(next);
+		}
+	}
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -20,30 +33,8 @@ int main()
 		tree[s].push_back(e);
 		tree[e].push_back(s);
 	}
-
-	deque<int> leaf;
-	while (!tree[1].empty())
-	{
-		for (int i = 1; i <= N; ++i)
-		{
-			for (int j = 0; j < leaf.size(); ++j)
-			{
-				for (int k = 0; k < tree[i].size(); ++k)
-				{
-					if (tree[i][k] == leaf[j])
-					{
-						tree[i].erase(tree[i].begin() + k);
-					}
-				}
-			}
-			
-			if (tree[i].size() == 1)
-			{
-				parent[i] = tree[i].front();
-				leaf.push_back(i);
-			}
-		}
-	}
+	parent[1] = 1;
+	dfs(1);
 
 	for (int i = 2; i <= N; ++i)
 	{
@@ -52,3 +43,4 @@ int main()
 
 	return 0;
 }
+// 진호야 나는 언제나 널 응원한단다 화이팅하자
