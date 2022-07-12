4764093d0d5acd0dfb98605392324e67fcf5149d
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-07-12 10:47:13 +0900

    Feat: 1991_TreeTraversal 성공
    1991_TreeTraversal
    성공

--- 1991_TreeTraversal/1991_TreeTraversal/TreeTraversal.cpp
+++ 1991_TreeTraversal/1991_TreeTraversal/TreeTraversal.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+vector<char> bst[26];
+void preorder(vector<char>& node)
+{
+	if(node.front() != '.')
+		cout << node.front();
+	if (node.size() == 1)
+		return;
+	if (node[1] != '.')
+		preorder(bst[node[1] - 'A']);
+	if (node[2] != '.')
+		preorder(bst[node[2] - 'A']);
+}
+
+void inorder(vector<char>& node)
+{
+	if (node[1] != '.')
+		inorder(bst[node[1] - 'A']);
+	if (node.front() != '.')
+		cout << node.front();
+	if (node[2] != '.')
+		inorder(bst[node[2] - 'A']);
+}
+
+void postorder(vector<char>& node)
+{
+	if (node[1] != '.')
+		postorder(bst[node[1] - 'A']);
+	if (node[2] != '.')
+		postorder(bst[node[2] - 'A']);
+	if (node.front() != '.')
+		cout << node.front();
+}
 
 int N;
 int main()
@@ -9,5 +42,24 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	cin >> N;
 
+	while (N--)
+	{
+		char parent, left, right;
+		cin >> parent >> left >> right;
+
+		bst[parent - 'A'].push_back(parent);
+		
+		bst[parent - 'A'].push_back(left);
+		bst[parent - 'A'].push_back(right);
+	}
+
+	preorder(bst[0]);
+	cout << '\n';
+	inorder(bst[0]);
+	cout << '\n';
+	postorder(bst[0]);
+
+	return 0;
 }
