e5209a7f5df8819b0922750c1702456b49aa7b7f
strange-tiger <jinolee9525@gmail.com>
2023-02-18 17:11:34 +0900

    Feat: 1620_PokemonMaster 다른 방식 성공
    문자열이 숫자인지 판단하는 방법을 찾기 귀찮아 시도했던 방법
    
    앞서 제출해보니 시간 초과가 났지만, 12, 13번 줄을 추가해 제출하니 통과하였다.

--- 1620_PokemonMaster/1620_PokemonMaster/1620_PokemonMaster.cpp
+++ 1620_PokemonMaster/1620_PokemonMaster/1620_PokemonMaster.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int N, M;
-string input;
-map<string, int> dictName;
-map<int, string> dictIndex;
+string input, index;
+map<string, bool> dictExist;
+map<string, string> dictIndex;
 int main(void)
 {
 	ios::sync_with_stdio(0);
@@ -17,19 +17,21 @@ int main(void)
 	for (int i = 1; i <= N; ++i)
 	{
 		cin >> input;
+		index = to_string(i);
 
-		dictName[input] = i;
-		dictIndex[i] = input;
+		dictExist[input] = true;
+		dictExist[index] = true;
+
+		dictIndex[input] = index;
+		dictIndex[index] = input;
 	}
 
 	for (int i = 0; i < M; ++i)
 	{
 		cin >> input;
-		
-		if (isdigit(input[0]))
-			cout << dictIndex[stoi(input)] << '\n';
-		else
-			cout << dictName[input] << '\n';
+
+		if (dictExist[input])
+			cout << dictIndex[input] << '\n';
 	}
 
 	return 0;
