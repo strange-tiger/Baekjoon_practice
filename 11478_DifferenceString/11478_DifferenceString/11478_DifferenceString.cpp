f04598cfe2b83ebb05fcdbe10af56614678a4a96
strange-tiger <jinolee9525@gmail.com>
2023-02-19 18:27:26 +0900

    Feat: 11478_DifferenceString 성공
    11478_DifferenceString 성공
    
    불필요한 for문 반복이 하나 추가되었던 것으로 생각된다. (k 변수 사용)
    메모리 사용량이 어마무시하다. set 사용으로 인한 것으로 생각된다. set을 여러 곳에 활용해보자.

--- 11478_DifferenceString/11478_DifferenceString/11478_DifferenceString.cpp
+++ 11478_DifferenceString/11478_DifferenceString/11478_DifferenceString.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <map>
+#include <set>
 using namespace std;
 
 string S, key;
 int SLen;
-map <string, bool> differGroup;
+set <string> differGroup;
 int main(void)
 {
 	ios::sync_with_stdio(false);
@@ -14,16 +14,13 @@ int main(void)
 	cin >> S;
 	SLen = S.size();
 
-	for (int i = SLen; i > 0; --i)
+	for (int i = 0; i < SLen; ++i)
 	{
-		for (int j = 0; j < i; ++j)
+		key.clear();
+		for (int j = i; j < SLen; ++j)
 		{
-			key.clear();
-
-			for (int k = 0; k < SLen - i + 1; ++k)
-				key.push_back(S[j + k]);
-
-			differGroup[key] = true;
+			key += S[j];
+			differGroup.insert(key);
 		}
 	}
 
