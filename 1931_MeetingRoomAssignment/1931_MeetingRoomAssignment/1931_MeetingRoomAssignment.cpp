b533d43946cb23c2528d5755fcce332ff7efce43
strange-tiger <jinolee9525@gmail.com>
2023-02-24 21:47:11 +0900

    Feat: 1931_MeetingRoomAssignment 성공
    1931_MeetingRoomAssignment 성공
    
    재귀를 사용할 필요가 없었다!
    올바른 규칙에 따라 선택하였으므로 이전 단계로 돌아가 반복할 필요가 없었다. 특히, 종료 시점을 기준으로 정렬하였으므로 최선의 선택은 항상 앞서 있는 선택지였던 것이다.
    
    다만, 이 과정에서 삽입 정렬은 시간을 너무 잡아먹게 되었다. 따라서 제공되는 퀵소트를 사용하기 위해 이차원 배열을 페어의 배열로 바꾸고 비교를 위해 이전에는 두 번째 칸에 넣었던 종료 시간을 첫 번째 칸, first에 넣었다.
    
    재귀의 활용에 주의할 필요가 있다. 그리고 자료구조 생각하기 귀찮아도 제공되는 sort를 활용할 방법을 생각해보는 것도 좋을 것 같다.

--- 1931_MeetingRoomAssignment/1931_MeetingRoomAssignment/1931_MeetingRoomAssignment.cpp
+++ 1931_MeetingRoomAssignment/1931_MeetingRoomAssignment/1931_MeetingRoomAssignment.cpp
@@ -1,48 +1,37 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int N;
-int Max = 0;
-int meeting[100000][2];
-void insertionSort()
-{
-	int tmp[2];
-	for (int i = 0; i < N - 1; ++i)
-	{
-		for (int j = i + 1; j > 0 && meeting[j][1] < meeting[j - 1][1]; --j)
-		{
-			tmp[0] = meeting[j][0];
-			tmp[1] = meeting[j][1];
-			meeting[j][0] = meeting[j - 1][0];
-			meeting[j][1] = meeting[j - 1][1];
-			meeting[j - 1][0] = tmp[0];
-			meeting[j - 1][1] = tmp[1];
-		}
-	}
-}
+typedef pair<int, int> meeting;
 
-void schedule(int index, int cnt)
-{
-	for (int i = index + 1; i < N; ++i)
-		if (meeting[index][1] <= meeting[i][0])
-			schedule(i, cnt + 1);
-
-	if (Max < cnt)
-		Max = cnt;
-}
-
+int N, S, E;
+int cnt = 1;
+meeting schedule[100000];
 int main(void)
 {
 	cin >> N;
 
 	for (int i = 0; i < N; ++i)
-		cin >> meeting[i][0] >> meeting[i][1];
+	{
+		cin >> S >> E;
+		schedule[i].first = E;
+		schedule[i].second = S;
+	}
 
-	insertionSort();
+	sort(schedule, schedule + N);
 
-	schedule(0, 1);
+	int endTime = 0;
+	for (int i = 1; i < N; ++i)
+	{
+		if (schedule[endTime].first <= schedule[i].second)
+		{
+			++cnt;
+			endTime = i;
+		}
+	}
 
-	cout << Max;
+	cout << cnt;
 
 	return 0;
 }
