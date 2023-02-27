78996f3bec111a09a2d7068a1dceca5de8fa3411
strange-tiger <jinolee9525@gmail.com>
2023-02-27 17:26:06 +0900

    Feat: 1912_ContinuousSum 실패
    1912_ContinuousSum 실패
    
    연속합은 즉 처음부터 지금까지의 합에서 그 전 어딘가까지의 합, 즉 이전 합의 차이다.
    따라서 이득을 볼 수 없는 항은 이전 합이 음수가 되는 항이므로, 그러한 항 그 다음부터 연속합을 계산하면 최대 연속합을 구하기 쉽다.
    
    ...라고 생각했는데 틀렸다.

--- 1912_ContinuousSum/1912_ContinuousSum/1912_ContinuousSum.cpp
+++ 1912_ContinuousSum/1912_ContinuousSum/1912_ContinuousSum.cpp
@@ -2,27 +2,23 @@
 using namespace std;
 
 int n, Max = -1001;
-int sum[100000] = { 0 };
+int now = 0;
 int main(void)
 {
 	cin >> n;
-
-	cin >> sum[0];
 
 	int tmp;
 	for (int i = 1; i < n; ++i)
 	{
 		cin >> tmp;
-		sum[i] = sum[i - 1] + tmp;
+		now += tmp;
 
-		if (Max < sum[i])
-			Max = sum[i];
+		if (Max < now)
+			Max = now;
+
+		if (now < 0)
+			now = 0;
 	}
-		
-	for (int i = 1; i < n; ++i)
-		for (int j = 0; j < i; ++j)
-			if (Max < sum[i] - sum[j])
-				Max = sum[i] - sum[j];
 
 	cout << Max;
 
