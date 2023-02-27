bcd90f41ba1fa73e2fcd54d2021106d79e05625f
strange-tiger <jinolee9525@gmail.com>
2023-02-27 20:23:53 +0900

    Feat: 1149_RGBStreet 성공
    1149_RGBStreet 성공
    
    색을 고를 때 고려해야 하는 것은 바로 이전 집과 바로 다음 집의 색 뿐이다.
    따라서 세 가지 색 중 하나를 고르면, 그 전 집이 다른 두 가지 색인 경우의 비용 중 최솟값이 더 작은 경우를 골라 이에 현재 비용을 더하면 된다.
    이를 N개의 집까지 수행하고, N번째 집이 어떤 색일 때 비용이 최소인지 비교하여 최솟값을 구한다.

--- 1149_RGBStreet/1149_RGBStreet/1149_RGBStreet.cpp
+++ 1149_RGBStreet/1149_RGBStreet/1149_RGBStreet.cpp
@@ -4,24 +4,21 @@ using namespace std;
 int N;
 int Min = 1000001;
 int house[1000][3];
-int painted[1000][3];
-int paint(int num, int cost, int prev)
+int painted[1001][3] = { 0 };
+void paint()
 {
-	if (num == N)
-		return cost;
-
-	int now;
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < N; ++i)
 	{
-		if (prev == i)
-			continue;
-
-		now = paint(num + 1, cost + house[num][i], i);
-		if (Min > now)
-			Min = now;
+		for (int j = 0; j < 3; ++j)
+		{
+			painted[i + 1][j] = min(painted[i][(j + 1) % 3], painted[i][(j + 2) % 3]);
+			painted[i + 1][j] += house[i][j];
+		}
 	}
 
-	return Min;
+	for (int cost : painted[N])
+		if (Min > cost)
+			Min = cost;
 }
 
 int main(void)
@@ -31,7 +28,7 @@ int main(void)
 	for (int i = 0; i < N; ++i)
 		cin >> house[i][0] >> house[i][1] >> house[i][2];
 
-	paint(0, 0, 3);
+	paint();
 
 	cout << Min;
 
