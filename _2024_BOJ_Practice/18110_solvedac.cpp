e6ba55dbd4195732c4f9a62afc39bd16fcc817d0
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2023-11-16 21:59:57 +0900

    Feat : 힙을 사용해 알고리즘 구성
    절사평균의 제외되는 큰 값과 작은 값을 각각, 큰 값은 최소힙, 작은 값은 최대힙을 만들어 루트 노드를 제하는 것으로 힙의 사이즈를 절사하는 갯수로 유지하는 방법을 시도해보았다.
    결과, 잘되는 것 같지만 값이 다르게 나온다.
    제거하는 과정에서 불필요하게 더해지는 모양.

--- _2024_BOJ_Practice/18110_solvedac.cpp
+++ _2024_BOJ_Practice/18110_solvedac.cpp
@@ -1,27 +1,73 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int N = 0;
+int cnt = 0;
 int cut = 0;
-int Max = 0, Min = 31;
+int cutCnt = 0;
+float fCut;
+
+priority_queue<int, vector<int>, greater<int>> bigPq;
+priority_queue<int> smallPq;
+
+int diffSum = 0;
 int result = 0;
-int difficultySum = 0;
+float fResult;
+
+int diff = 0;
 
 int main()
 {
 	cin >> N;
 
-	if (N)
+	if (N == 0)
 	{
 		cout << result;
 		return 0;
 	}
 
-	float bcut = N * 0.15f;
-	cut = (int)bcut;
+	fCut = N * 0.15f;
+	cut = (int) fCut;
 
-	if (bcut - cut >= 0.5f)
+	if (fCut - cut >= 0.5f)
 		++cut;
 
+	while (N > cnt)
+	{
+		++cnt;
 
+		cin >> diff;
+		
+		bigPq.push(diff);
+		smallPq.push(diff);
+
+		if (cutCnt < cut)
+		{
+			++cutCnt;
+
+			continue;
+		}
+
+		if (bigPq.top() == smallPq.top())
+		{
+			diffSum += bigPq.top();
+		}
+		else
+		{
+			diffSum += bigPq.top();
+			diffSum += smallPq.top();
+		}
+
+		bigPq.pop();
+		smallPq.pop();
+	}
+
+	fResult = (float) diffSum / (N - 2 * cut);
+	result = (int) fResult;
+
+	if (fResult - result >= 0.5f)
+		++result;
+
+	cout << result;
 }
