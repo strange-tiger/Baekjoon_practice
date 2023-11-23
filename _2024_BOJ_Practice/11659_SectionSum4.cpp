2ab333fbb8820d86f11997345ea4edd262d8a05f
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2023-11-23 17:14:44 +0900

    Fix : 11659_SectionSum4 실패 2
    어디서 시간초과가 나는지 모르겠다. 최대한 간결히 해보았는데 시간초과가 났다. c로 바꿔서 해봐야겠다.

--- _2024_BOJ_Practice/11659_SectionSum4.cpp
+++ _2024_BOJ_Practice/11659_SectionSum4.cpp
@@ -2,26 +2,23 @@
 using namespace std;
 
 int N, M;
-int i, j, temp;
+int i, j;
 int sum[100001] = { 0, };
 
 int main()
 {
 	cin >> N >> M;
 
-	sum[0] = 0;
-
 	for (int k = 1; k <= N; ++k)
 	{
-		cin >> temp;
-		sum[k] = temp + sum[k - 1];
+		cin >> sum[k];
+		sum[k] += sum[k - 1];
 	}
 
 	while (M--)
 	{
 		cin >> i >> j;
-		temp = sum[j] - sum[i - 1];
-		cout << temp << endl;
+		cout << sum[j] - sum[i - 1] << endl;
 	}
 
 	return 0;
