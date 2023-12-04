22ed8bb1dbf63e2af1a36dced5b26b26f3be502c
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2023-12-04 14:05:31 +0900

    Feat : 14940_EasyShortestDistance 해결
    답을 저장하는 dist 배열에 답이 있는가로 isVisited의 방문여부 저장을 대신하였다.
    bfs에서 큐를 사용함은 다음 방문할 노드를 기억하기 위함을 다시 알았다.
    memset은 string이나 memory 헤더를 사용해야 한다.

--- _2024_BOJ_Practice/14940_EasyShortestDistance.cpp
+++ _2024_BOJ_Practice/14940_EasyShortestDistance.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <algorithm>
+#include <utility>
 #include <queue>
+#include <cstring>
 using namespace std;
 
 int n, m;
 int map[1000][1000];
-int distance[1000][1000];
-bool isVisited[1000][1000];
-
+int dist[1000][1000];
+queue<pair<int, int>> q;
 void bfs()
 {
 	static int dx[] = { 1, 0, -1, 0 };
 	static int dy[] = { 0, 1, 0, -1 };
+
+	int x, y, nx, ny;
+	while (!q.empty())
+	{
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; ++i)
+		{
+			nx = x + dx[i];
+			ny = y + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m || dist[nx][ny] != -1 || map[nx][ny] == 0)
+				continue;
+
+			dist[nx][ny] = dist[x][y] + 1;
+			q.push({ nx, ny });
+		}
+	}
 }
 
 int main()
@@ -20,13 +41,40 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	for (int i = 0; i < 1000; ++i)
+	{
+		memset(map[i], 0, 1000 * sizeof(int));
+		memset(dist[i], -1, 1000 * sizeof(int));
+	}
+
 	cin >> n >> m;
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
 		{
 			cin >> map[i][j];
+
+			if (map[i][j] == 2)
+			{
+				dist[i][j] = 0;
+				q.push({ i, j });
+			}
 		}
+	}
+
+	bfs();
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (map[i][j] == 0)
+				cout << 0 << ' ';
+			else
+				cout << dist[i][j] << ' ';
+		}
+
+		cout << '\n';
 	}
 
 	return 0;
