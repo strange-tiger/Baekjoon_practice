29350de028087bd046680cfacc364aceed1b8056
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2023-12-05 13:41:33 +0900

    Feat : 1043_Lie 해결
    진실을 아는 사람과 만나지 않은 사람만으로 이루어진 파티에서 거짓말을 할 수 있다.
    여기서 사람들이 서로 만난 그룹인지를 판별해야하는데, 그 과정에서 매번 순회탐색을 하는 건 시간이 부족할 것 같았다.
    그래서 찾아보니 유니온 파인드(Union Find)라는 것으로 원소가 속해있는 집합을 구별할 수 있다는 걸 알았고 적용했다.
    즉, 각 인덱스를 사람이라 하고 만난 사람 중 가장 인덱스가 작은 것을 원소로 가진다 할 때, 0번을 진실을 아는 사람의 인덱스라 하면 진실을 알게된 모든 사람은 0을 원소로 갖게 된다.
    후에 블로그에 유니온 파인드를 공부해 정리해야겠다.
    https://hagisilecoding.tistory.com/122

---
+++ _2024_BOJ_Practice/1043_Lie.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+int knowTruth;
+int parent[53]; 
+// 각 번호를 그룹 이름으로, 만난 사람을 원소로 갖는 그룹을 만든다.
+// 0번은 진실을 아는 사람
+int temp;
+vector<int> party[53];
+int partyNum;
+
+int lie;
+
+// 유니온 파인드(Union Find) : 각 원소가 속해있는 집합을 구별하고 찾기 위한 방법
+int findParent(int x)
+{
+	if (parent[x] != x)
+		return parent[x] = findParent(parent[x]);
+	return x;
+}
+
+void merge(int a, int b)
+{
+	int x = findParent(a);
+	int y = findParent(b);
+
+	if (x != y)
+	{
+		if (x < y) // 그룹 번호가 더 작은 쪽으로 합친다.
+			parent[y] = x;
+		else
+			parent[x] = y;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N >> M;
+	cin >> knowTruth;
+
+	for (int i = 0; i <= N; ++i)
+		parent[i] = i;
+
+	while (knowTruth--)
+	{
+		cin >> temp;
+		parent[temp] = 0;
+	}
+
+	for (int i = 0; i < M; ++i)
+	{
+		cin >> partyNum;
+		cin >> temp;
+		party[i].push_back(temp);
+
+		for (int j = 1; j < partyNum; ++j)
+		{
+			cin >> temp;
+			party[i].push_back(temp);
+			merge(party[i][0], party[i][j]);
+		}
+	}
+
+	lie = M;
+
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j : party[i])
+		{
+			if (findParent(parent[j]) == 0)
+			{
+				--lie;
+				break;
+			}
+		}
+	}
+
+	cout << lie;
+
+	return 0;
+}
