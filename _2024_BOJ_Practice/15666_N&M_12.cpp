f591b4e41bcef73c93d1b296cb3d9534fdb7635c
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2023-12-24 15:01:33 +0900

    Feat : 15666_N&M_12 해결
    N&M 8번과 9번을 섞은 문제이다.
    간단히 조건을 겹쳐서 해결하려 했는데 마지막에 꼭 중복수열이 나왔다.
    그래서 함수를 바꿔서 이전 레벨 num 부터 시작하는 것으로 바꾸니 해결했다. 이유는 8번의 해결방식 그대로 하면 받은 수 중 중복된 수를 별개로 취급하게 돼서 그런 것으로 생각된다.

---
+++ _2024_BOJ_Practice/15666_N&M_12.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int num[8];
+int seq[8];
+void dfs(int x, int depth)
+{
+    if (depth == M)
+    {
+        for (int i = 0; i < M; ++i)
+            cout << seq[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int temp = 0;
+    for (int i = x; i < N; ++i)
+    {
+        if (num[i] != temp)
+        {
+            seq[depth] = num[i];
+            temp = seq[depth];
+            dfs(i, depth + 1);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> num[i];
+    }
+
+    sort(num, num + N);
+
+    dfs(0, 0);
+
+    return 0;
+}
