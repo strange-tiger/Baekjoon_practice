f99473e91b876980e77b00ca460b053a34a7b69e
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2023-12-30 00:08:42 +0900

    Feat : 2504_ValueOfBracket 해결
    처음에는 값을 저장하는 스택을 만들어서 괄호로 묶일 때마다 스택에 넣고 한꺼번에 더해서 곱해주고 뭐 이런 식으로 조건을 수없이 만들어 풀려했다. 당연히 안 풀렸다. 그래서 수식으로 바꿔보니, 예를 들어 (()[[]]) 같은 괄호열은 2 * (2 + 3 * 3) 인데, 이거 분배 법칙으로 2 * 2 + 2 * 3 * 3 이더라. 그냥 괄호 빠져 나올 때 value를 나누고 다시 계산하고 하는 방식으로 해결이 되었다.
    여기서 또 시행착오를 겪은 것이, 굳이 for ( char c : br) 뭐 이렇게 소위 개선된 for문을 쓰고 싶어서 낑낑대다가 어찌해도 그냥 for문이 편해서 이렇게 했다. 굳이 아는 거 다 쓰지 않는 게 편할 때도 있다, 혹은 쓸 때를 알자고 하는 교훈을 얻었다.
    다음은 14719번을 풀까 한다.

---
+++ _2024_BOJ_Practice/2504_ValueOfBracket.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int value = 1;
+int result = 0;
+string br;
+stack<char> st;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> br;
+
+	for (int i = 0; i < br.length(); ++i)
+	{
+		if (br[i] == '(')
+		{
+			value *= 2;
+			st.push(br[i]);
+		}
+		else if (br[i] == '[')
+		{
+			value *= 3;
+			st.push(br[i]);
+		}
+		else if (br[i] == ')')
+		{
+			if (st.empty() || st.top() != '(')
+			{
+				result = 0;
+				break;
+			}
+			if (br[i - 1] == '(')
+			{
+				result += value;
+			}
+
+			value /= 2;
+			st.pop();
+		}
+		else if (br[i] == ']')
+		{
+			if (st.empty() || st.top() != '[')
+			{
+				result = 0;
+				break;
+			}
+			if (br[i - 1] == '[')
+			{
+				result += value;
+			}
+
+			value /= 3;
+			st.pop();
+		}
+	}
+
+	if (!st.empty())
+		result = 0;
+
+	cout << result;
+
+	return 0;
+}
