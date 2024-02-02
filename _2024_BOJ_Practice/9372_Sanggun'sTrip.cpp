93657d088d8c9ed29a76e8b5feaf93d77aee144c
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-02-02 18:49:04 +0900

    Feat : 9372_Sanggun'sTrip 해결

---
+++ _2024_BOJ_Practice/9372_Sanggun'sTrip.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int T, N, M, a, b;
+int main()
+{
+    cin >> T;
+    while (T--)
+    {
+        cin >> N >> M;
+        while (M--)
+        {
+            cin >> a >> b;
+        }
+
+        cout << N - 1 << '\n';
+    }
+    return 0;
+}
