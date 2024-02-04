5769f349ab42f7832a7c8a29b9e66eccfd101ed8
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-02-04 15:29:56 +0900

    Feat : 1865_Wormhole 해결
    벨만-포드라는 새로운 알고리즘을 배워 문제를 해결했다. 벨만-포드는 다익스트라와 달리 음의 가중치가 있는 그래프에서도 사용가능하다. 그 중에서 사이클을 순회할 때마다 거리가 짧아지는 음의 사이클이 있는지 여부를 찾는 것으로 문제를 해결했다.

---
+++ _2024_BOJ_Practice/1865_Wormhole.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+#include <utility>
+#define INF 1000000000
+using namespace std;
+
+int TC, N, M, W;
+int S, E, T;
+int i, j;
+bool cycle = false;
+int dist[501];
+vector<pair<int, int>> road[501];
+
+void Init()
+{
+	for (i = 1; i <= N; ++i)
+	{
+		dist[i] = INF;
+		road[i].clear();
+	}
+}
+
+bool Bellman_Ford(int n)
+{
+	for (i = 1; i <= n; ++i)
+		for (j = 1; j <= n; ++j)
+			for (pair<int, int> way : road[j])
+				if (dist[way.first] > way.second + dist[j])
+					dist[way.first] = way.second + dist[j];
+
+	for (i = 1; i <= n; ++i) // 음의 사이클 찾기
+		for (pair<int, int> way : road[i])
+			if (dist[way.first] > way.second + dist[i])
+				return true;
+
+	return false;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> TC;
+	
+	while (TC--)
+	{
+		Init();
+
+		cin >> N >> M >> W;
+
+		dist[1] = 0;
+
+		for (i = 0; i < M; ++i)
+		{
+			cin >> S >> E >> T;
+			road[S].push_back({ E, T });
+			road[E].push_back({ S, T });
+		}
+
+		for (i = 0; i < W; ++i)
+		{
+			cin >> S >> E >> T;
+			road[S].push_back({ E, -T });
+		}
+
+		if (Bellman_Ford(N))
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+
+	return 0;
+}
