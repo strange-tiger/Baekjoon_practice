656bcda52a71d907746d6ccb6aa989a75c039caf
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-03-09 12:26:38 +0900

    Feat : 17070_MovePipe1_dp 해결
    찬찬히 다시 생각해서 dp를 계산하니 생각보다 간단히 구축할 수 있었다. 요는 현재 파이프 방향에 따라 가로면 이전 대각선과 가로, 세로면 이전 대각선과 세로, 대각선은 세 방향 모두가 가능하므로 이들의 합을 각각 구해서 넣어주는 것이다.
    시간이 dfs를 사용한 경우보다 훨씬 빨라졌다.

--- _2024_BOJ_Practice/17070_MovePipe1_dp.cpp
+++ _2024_BOJ_Practice/17070_MovePipe1_dp.cpp
@@ -1,2 +1,50 @@
 #include <iostream>
 using namespace std;
+
+int N, i, j;
+int house[16][16] = { 0 };
+int dp[16][16][3] = { 0 };
+
+// dp[y][x][dir] dir : 0 -> 가로 1 -> 세로 2 -> 대각선
+int DP()
+{
+	dp[0][1][0] = 1;
+
+	for (i = 0; i < N; ++i)
+	{
+		for (j = 2; j < N; ++j)
+		{
+			if (!house[i][j])
+			{
+				dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+
+				if (i > 0)
+				{
+					dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+
+					if (!house[i][j - 1] && !house[i - 1][j])
+						dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+				}
+			}
+		}
+	}
+
+	return dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (i = 0; i < N; ++i)
+		for (j = 0; j < N; ++j)
+			cin >> house[i][j];
+
+	cout << DP();
+
+	return 0;
+}
