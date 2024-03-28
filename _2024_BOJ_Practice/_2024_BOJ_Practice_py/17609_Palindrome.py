a1aca5728591f2e820e61131f7344b25b99317d4
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-03-28 21:57:09 +0900

    Fix : 17609_Palindrome.py 해결
    함수로 기능을 구분.
    슬라이싱과 인덱싱을 활용해 더 빠르고 간단하게 회문 여부 판정.
    유사회문은 투 포인터가 같은 글자를 더 이상 가르키지 않을 때 그 글자를 제외하고 순방향과 역방향 문자열을 비교하는(즉, 회문 판정 반복) 것으로 판정
    성공! return 2가 한 칸 더 앞에 가서 1이 영 안 나오는 해프닝이 있었다.

--- _2024_BOJ_Practice/_2024_BOJ_Practice_py/17609_Palindrome.py
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/17609_Palindrome.py
@@ -2,51 +2,29 @@
 
 import sys
 
+def isPalindrome (s = str()) :
+    if s == s[::-1] :
+        return 0
+    else :
+        begin = 0
+        end = len(s) - 1
+        while begin < end :
+            if s[begin] == s[end] :
+                begin += 1
+                end -= 1
+            else :
+                if s[begin] == s[end - 1] :
+                    temp = s[begin:end]
+                    if temp == temp[::-1] :
+                        return 1
+                if s[begin + 1] == s[end] :
+                    temp = s[begin + 1 : end + 1]
+                    if temp == temp[::-1] :
+                        return 1
+                return 2
+
 T = int(sys.stdin.readline().rstrip())
 
 for _ in range(T) :
-    BREAK = False
-    s = sys.stdin.readline().rstrip()
-
-    begin = 0
-    end = len(s) - 1
-
-    while s[begin] == s[end] :
-        begin += 1
-        end -= 1
-
-        if begin == end or begin - end == 1 :
-            BREAK = True
-            print(0)
-            break
-    if BREAK :
-        continue
-
-    for i in range(len(s)) :
-        begin = 0
-        end = len(s) - 1
-
-        if i == begin :
-            begin += 1
-        elif i == end :
-            end -= 1
-
-        while s[begin] == s[end] :
-            begin += 1
-            end -= 1
-
-            if i == begin :
-                begin += 1
-            elif i == end :
-                end -= 1
-            #print(begin, end)
-            if begin == end or begin - end == 1 :
-                BREAK = True
-                print(1)
-                break
-        if BREAK :
-            break
-    if BREAK :
-            continue
-
-    print(2)
+    S = sys.stdin.readline().rstrip()
+    print(isPalindrome(S))
