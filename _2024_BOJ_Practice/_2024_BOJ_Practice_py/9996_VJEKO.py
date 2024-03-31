124be247e2b2abf83ed6d33a26f05f38d6f2de9a
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-03-31 15:27:43 +0900

    Fix : 9996_VJEKO.py 해결
    예외처리 하나를 깜빡했다. 비교 대상이 begin과 end를 합한 것보다 짧으면 패턴과 일치할 수가 없다.

--- _2024_BOJ_Practice/_2024_BOJ_Practice_py/9996_VJEKO.py
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/9996_VJEKO.py
@@ -3,10 +3,14 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 
 begin, end = sys.stdin.readline().rstrip().split('*')
+L = len(begin) + len(end)
 
 for _ in range(N) :
     txt = sys.stdin.readline().rstrip()
-    if txt[:len(begin)] == begin and txt[len(txt) - len(end):] == end :
-        print("DA")
-    else :
+    if len(txt) < L :
         print("NE")
+    else :
+        if txt[:len(begin)] == begin and txt[-len(end):] == end :
+            print("DA")
+        else :
+            print("NE")
