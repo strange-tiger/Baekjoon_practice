880b707436d7b1361c83fc75ac85dba7290ee07f
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-04-21 13:42:34 +0900

    Feat : 1783_SickKnight 실패
    조건에 따라 분기하여 결과를 내도록 하면 간단한 문제였다.
    예외 처리가 부족했던 모양이다. 실패

---
+++ _2024_BOJ_Practice/1783_SickKnight.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	if (N == 1)
+		cout << 1;
+	else if (N == 2)
+		cout << min(4, M + 1);
+	else if (M < 7)
+		cout << min(4, M);
+	else
+		cout << M - 2;
+	return 0;
+}
