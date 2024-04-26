5b73ea6fb924bbfe6ab840fa1b84670d6e34bc30
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-04-26 21:27:13 +0900

    Fix : 2012_Ranking.py 해결
    기존 두번째 방법은 중간 빈 수열을 해결하지 못했다. 그리고 한다고 해도 첫번째 방법보다 같거나 커질 수 밖에 없었다. 이는 수학적으로 올바른 수열 모든 요소의 합과 비교해보는 것으로 알 수 있다.
    따라서 앞 방법만 남기고 정리하니 맞았다.

--- _2024_BOJ_Practice/_2024_BOJ_Practice_py/2012_Ranking.py
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/2012_Ranking.py
@@ -8,17 +8,8 @@ for _ in range(N):
 
 rank.sort()
 
-ansA = 0
+ans = 0
 for i in range(N):
-    ansA += abs(i - rank[i] + 1)
+    ans += abs(i - rank[i] + 1)
 
-ansB = 0
-rankB = list()
-for i in range(N):
-    if rank[i] != i + 1:
-        rankB.append(rank[i])
-
-for i in range(len(rankB)):
-    ansB += i + 1 + len(rank) - len(rankB) - rankB[i]
-
-print(min(ansA, ansB))
+print(ans)
