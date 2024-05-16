ad9c4edd530f91d8db7b4fb1d05ca6c28b3ca31a
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-05-16 09:23:06 +0900

    Feat : 12781_PIZZA_ALVOLOC 해결
    17386 선분교차1과 같은 방법을 응용할 수 있었다. 4조각이 되려면 4점으로 만들어진 두 선분이 교차해야하기 때문.

---
+++ _2024_BOJ_Practice/12781_PIZZA_ALVOLOC.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int point[4][2];
+
+void input()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 0; i < 4; ++i)
+		cin >> point[i][0] >> point[i][1];
+}
+
+int ccw(int a[2], int b[2], int c[2])
+{
+	int tmp = a[0] * b[1] + b[0] * c[1] + c[0] * a[1];
+	tmp -= a[0] * c[1] + b[0] * a[1] + c[0] * b[1];
+
+	if (tmp > 0)
+		return 1;
+	else if (tmp == 0)
+		return 0;
+	else
+		return -1;
+}
+
+void solve()
+{
+	int line1 = ccw(point[0], point[1], point[2]) * ccw(point[0], point[1], point[3]);
+	int line2 = ccw(point[0], point[2], point[3]) * ccw(point[1], point[2], point[3]);
+
+	if (line1 < 0 && line2 < 0)
+		cout << 1;
+	else
+		cout << 0;
+}
+
+int main()
+{
+	input();
+	solve();
+	return 0;
+}
