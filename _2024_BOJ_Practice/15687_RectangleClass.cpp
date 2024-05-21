b711d03b287ee64acd03502b9ceaeee1b762358d
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-05-21 10:51:23 +0900

    Feat : 15687_RectangleClass 해결
    백준 문제 중에 처음 보는 제출 형식. 클래스를 구현 해 제출해야 한다.
    간단히 구현할 수 있었다.

---
+++ _2024_BOJ_Practice/15687_RectangleClass.cpp
@@ -0,0 +1,44 @@
+class Rectangle
+{
+	private:
+		int width;
+		int height;
+	public:
+		Rectangle(int width, int height)
+		{
+            set_width(width);
+            set_height(height);
+		}
+        int get_width() const
+        {
+            return width;
+        }
+        int get_height() const
+        {
+            return height;
+        }
+        void set_width(int width)
+        {
+            if (width <= 0 || width > 1000)
+                return;
+            this->width = width;
+        }
+        void set_height(int height)
+        {
+            if (height <= 0 || height > 2000)
+                return;
+            this->height = height;
+        }
+        int area() const
+        {
+            return width * height;
+        }
+        int perimeter() const
+        {
+            return 2 * (width + height);
+        }
+        bool is_square() const
+        {
+            return width == height;
+        }
+};
