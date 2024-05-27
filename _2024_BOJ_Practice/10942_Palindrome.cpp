7eee64851a6550c199e844e61d36ef9dbbe708d1
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-05-27 16:09:56 +0900

    Fix : 10942_Palindrome 해결
    숫자 실수였었다. 고치고 해결하였다.

--- _2024_BOJ_Practice/10942_Palindrome.cpp
+++ _2024_BOJ_Practice/10942_Palindrome.cpp
@@ -29,8 +29,8 @@ void solve()
 			dp[i][i + 1] = false;
 	}
 
-	for (int i = 0; i < N - 2; ++i)
-		for (int j = i + 2; j < N; ++j)
+	for (int i = N - 1; i >= 0; --i)
+		for (int j = i + 1; j < N; ++j)
 			if (seq[i] == seq[j] && dp[i + 1][j - 1])
 				dp[i][j] = true;
 
