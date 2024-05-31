4a7e0dcc2151dcf210e66db1dd75f4264af13762
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-05-31 09:03:10 +0900

    Feat : 25682_RepaintChessboard2 진행
    2년 전에 푼 1018번 체스판 다시 칠하기 문제의 발전형이다. 8*8 크기 판에서 임의의 K*K 크기 판으로, 또 N, M이 50에서 2000까지 늘어났다. 큰 수에 주의하며 코드를 변경했다.
    디버깅에서는 문제가 없으나, 범위가 커짐에 따라 소요시간이 엄청 늘었을 것이고, 원래 소스코드는 O(n^4)의 엄청 느린 코드이므로 수정해야만 한다.
    현재 예상으로는 누적합의 2차원 배열을 2개 준비해 왼쪽 위가 흰색인 경우와 검은색인 경우, 해당 위치까지의 판의 고쳐야할 칸 수를 저장하는 것으로 시도해보려 한다.

---
+++ _2024_BOJ_Practice/25682_RepaintChessboard2.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int N, M, K, MIN;
+string board[2000];
+
+void input()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M >> K;
+
+	for (int i = 0; i < N; ++i)
+		cin >> board[i];
+}
+
+void solve()
+{
+	MIN = 2e6;
+	for (int i = 0; i <= N - K; ++i)
+	{
+		for (int j = 0; j <= M - K; ++j)
+		{
+			// 왼쪽 위가 하얀색인 경우
+			int cnt = 0;
+			for (int k = i; k < i + K; ++k)
+			{
+				for (int l = j; l < j + K; ++l)
+				{
+					if ((k + l) % 2 == 1 && board[k][l] == 'W')
+						++cnt;
+					else if ((k + l) % 2 == 0 && board[k][l] == 'B')
+						++cnt;
+
+				}
+			}
+
+			if (MIN > cnt)
+				MIN = cnt;
+
+			// 왼쪽 위가 검은색인 경우
+			cnt = 0;
+			for (int k = i; k < i + K; ++k)
+			{
+				for (int l = j; l < j + K; ++l)
+				{
+					if ((k + l) % 2 == 1 && board[k][l] == 'B')
+						++cnt;
+					else if ((k + l) % 2 == 0 && board[k][l] == 'W')
+						++cnt;
+
+				}
+			}
+
+			if (MIN > cnt)
+				MIN = cnt;
+		}
+	}
+
+	cout << MIN;
+}
+
+int main()
+{
+	input();
+	solve();
+	return 0;
+}
