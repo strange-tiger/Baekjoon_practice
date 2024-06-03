74f8348ee4ef8242f764a2c4842816c38456e3d7
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-04 08:23:24 +0900

    Feat : 11049_MatrixMultiplicationOrder 해결
    이 문제에는 계속 변하면서 값을 저장해놔야 하는 변수가 두 가지 있다. 답이 될 연산 횟수와 연산과 함께 변화하는 행렬이다. 따라서 dp배열을 두 개 준비해 적용해야 한다. 이것이 첫 번째 핵심이었다.
    
    dp에서 앞 인덱스는 시작 행렬을, 뒤
     인덱스는 끝 행렬을 가리키는 방식으로 시도해보았다. 즉, dp[s][e]는 s번째 행렬부터 시작해 e번째 행렬까지 연산한 결과, 연산 횟수가 될 것이다. 이 dp의 의미를 아는 것이 두 번째 핵심이었다. 같은 이유로 dpMat[s][e]는 s부터 e까지 계산한 다음의 행렬 값이 된다.
    
    연산을 할 때 순서대로 연산한다면 문제 없겠지만, 뒤의 행렬을 먼저 곱셉하여 구한다면 앞과 뒤를 따로 묶어 연산해야만 한다. 따라서 기준값을 정해 이를 기준으로 앞과 뒤의 행렬을 곱하는 것으로 해답을 구할 수 있을 것이다. 즉, dp[s][e] = dp[s][tmp] + dp[tmp + 1][e] + mat[s][0] * mat[tmp][1] * mat[e][1]이다. 이 점화식을 구하는 게 세 번째 핵심이었다.
    
    중간중간의 고민과 시도를 기록하려고 브랜치를 새로 팠는데 문제에 열중한 나머지 커밋을 못했다. 다음에는 자주 커밋을 해보자.

--- _2024_BOJ_Practice/11049_MatrixMultiplicationOrder.cpp
+++ _2024_BOJ_Practice/11049_MatrixMultiplicationOrder.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <algorithm>
 #define MAX 2147483647
 using namespace std;
 
 int N;
-int mat[500][2];
-int dp[500][500];
+int mat[501][2];
+int dp[501][501];
+int dpMat[501][501][2];
+
+void proj(int m, int n)
+{
+	dpMat[m][n][0] = mat[m][0];
+	dpMat[m][n][1] = mat[n][1];
+}
 
 void input()
 {
@@ -17,12 +25,30 @@ void input()
 	for (int i = 0; i < N; ++i)
 	{
 		cin >> mat[i][0] >> mat[i][1];
+		proj(i, i);
 	}
 }
 
 void solve()
 {
+	for (int i = 1; i < N; ++i)
+	{
+		for (int s = 0; s + i < N; ++s)
+		{
+			int e = s + i;
 
+			dp[s][e] = MAX;
+
+			for (int mid = s; mid < e; ++mid)
+			{
+				dp[s][e] = min(dp[s][e], dp[s][mid] + dp[mid + 1][e] + dpMat[s][mid][0] * dpMat[s][mid][1] * dpMat[mid + 1][e][1]);
+			}
+
+			proj(s, e);
+		}
+	}
+
+	cout << dp[0][N - 1];
 }
 
 int main()
