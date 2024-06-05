9413dcb8a3005afe1cf0279630bfc6a553355fc9
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-05 18:25:39 +0900

    Fix : 30805_LexicographicallyLCS.py 실패
    가장 나중인 수열이라함은, 결국 공통 요소 중 가장 큰 수를 맨앞에 둔 감소 수열이다. 공통 요소를 앞서 알게 된 set의 교집합을 구하는 방법으로 구하고, 그 중 최대 숫자부터 리스트에 넣고 큰 수부터 최대 숫자보다 인덱스가 뒤이면 결과 리스트에 넣어주었다.
    
    ...이게 왜 틀렸을까

--- _2024_BOJ_Practice/_2024_BOJ_Practice_py/30805_LexicographicallyLCS.py
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/30805_LexicographicallyLCS.py
@@ -6,20 +6,11 @@ A = list(map(int, sys.stdin.readline().rstrip().split()))
 M = int(sys.stdin.readline().rstrip())
 B = list(map(int, sys.stdin.readline().rstrip().split()))
 
-dp = [[0 for j in range(len(B))] for i in range(len(A))]
-LCS = list()
+subseq = list(set(A) & set(B))
 
-for i in range(0, len(A)):
-    for j in range(0, len(B)):
-        if A[i] == B[j]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-            LCS.append(A[i])
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+LLCS = [subseq[-1]]
+for i in range(2, len(subseq) + 1):
+    if A.index(subseq[-i]) > A.index(LLCS[-1]) and B.index(subseq[-i]) > B.index(LLCS[-1]):
+        LLCS.append(subseq[-i])
 
-subseq = list()
-for i in range(len(LCS) + 1):
-    subseq += list(combinations(LCS, i))
-subseq.sort()
-
-print(*subseq[len(subseq) - 1])
+print(*LLCS)
