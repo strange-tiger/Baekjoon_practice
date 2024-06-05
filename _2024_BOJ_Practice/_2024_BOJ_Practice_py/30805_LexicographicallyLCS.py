2b4368290a2b8995154b88703edf1564ac61a0b3
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-05 19:39:50 +0900

    Fix : 30805_LexicographicallyLCS.py 실패
    공집합을 고려하지 않았다는 걸 알았다! LLCS는 공집합이 될 수 있다. 이를 예외 처리를 하면 그 외에 예외 처리가 필요한 부분이 없을 것이다.
    
    62%에서 실패. 미리 브랜치를 따로 파둘 걸 그랬다.

--- _2024_BOJ_Practice/_2024_BOJ_Practice_py/30805_LexicographicallyLCS.py
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/30805_LexicographicallyLCS.py
@@ -7,18 +7,21 @@ B = list(map(int, sys.stdin.readline().rstrip().split()))
 
 intersec = list(set(A) & set(B))
 
-LLCS = [intersec[-1]]
-idxA = A.index(LLCS[-1])
-idxB = B.index(LLCS[-1])
-while len(intersec) > 0:
-    if idxA == len(A) - 1 or idxB == len(B) - 1:
-        break
-    if intersec[-1] in A[idxA + 1:] and intersec[-1] in B[idxB + 1:]:
-        LLCS.append(intersec[-1])
-        idxA += A[idxA + 1:].index(LLCS[-1]) + 1
-        idxB += B[idxB + 1:].index(LLCS[-1]) + 1
-    else:
-        intersec.remove(intersec[-1])
+if len(intersec) > 0:
+    LLCS = [intersec[-1]]
+    idxA = A.index(LLCS[-1])
+    idxB = B.index(LLCS[-1])
+    while len(intersec) > 0:
+        if idxA == len(A) - 1 or idxB == len(B) - 1:
+            break
+        if intersec[-1] in A[idxA + 1:] and intersec[-1] in B[idxB + 1:]:
+            LLCS.append(intersec[-1])
+            idxA += A[idxA + 1:].index(LLCS[-1]) + 1
+            idxB += B[idxB + 1:].index(LLCS[-1]) + 1
+        else:
+            intersec.remove(intersec[-1])
+else:
+    LLCS = []
 
 print(len(LLCS))
 print(*LLCS)
