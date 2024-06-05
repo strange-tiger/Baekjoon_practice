96b19b7e1cf80a724e956bfecf0cf8924db60e4a
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-05 13:54:30 +0900

    Feat : 31403_A+B-C.py 해결
    문자열의 정수형으로의 변환을 사용하는 문제

---
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/31403_A+B-C.py
@@ -0,0 +1,11 @@
+import sys
+
+A = sys.stdin.readline().rstrip()
+B = sys.stdin.readline().rstrip()
+C = sys.stdin.readline().rstrip()
+
+num = int(A) + int(B) - int(C)
+print(num)
+
+string = int(A + B) - int(C)
+print(string)
