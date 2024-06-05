d415262af9d6f9bfc254607318583bfa73bb4bdb
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-05 19:29:47 +0900

    Fix : 30805_LexicographicallyLCS.py 실패
    LLCS 들어간 숫자의 인덱스를 idxA, idxB에 따로 저장하는 것으로 중복 숫자 또한 고려할 수 있게 수정했다. 다음 인덱스는 나온 인덱스를 더 더하는 것으로 해결할 수 있다. 디버그 결과는 올바르게 나왔다.
    또 틀렸네. 테스트 케이스부터 찾아봐야겠다.

--- _2024_BOJ_Practice/_2024_BOJ_Practice_py/30805_LexicographicallyLCS.py
+++ _2024_BOJ_Practice/_2024_BOJ_Practice_py/30805_LexicographicallyLCS.py
@@ -1,17 +1,24 @@
 import sys
-from itertools import combinations
 
 N = int(sys.stdin.readline().rstrip())
 A = list(map(int, sys.stdin.readline().rstrip().split()))
 M = int(sys.stdin.readline().rstrip())
 B = list(map(int, sys.stdin.readline().rstrip().split()))
 
-subseq = list(set(A) & set(B))
+intersec = list(set(A) & set(B))
 
-LLCS = [subseq[-1]]
-for i in range(2, len(subseq) + 1):
-    if subseq[-i] in A[A.index(LLCS[-1]):] and subseq[-i] in B[B.index(LLCS[-1]):]:
-        LLCS.append(subseq[-i])
+LLCS = [intersec[-1]]
+idxA = A.index(LLCS[-1])
+idxB = B.index(LLCS[-1])
+while len(intersec) > 0:
+    if idxA == len(A) - 1 or idxB == len(B) - 1:
+        break
+    if intersec[-1] in A[idxA + 1:] and intersec[-1] in B[idxB + 1:]:
+        LLCS.append(intersec[-1])
+        idxA += A[idxA + 1:].index(LLCS[-1]) + 1
+        idxB += B[idxB + 1:].index(LLCS[-1]) + 1
+    else:
+        intersec.remove(intersec[-1])
 
 print(len(LLCS))
 print(*LLCS)
