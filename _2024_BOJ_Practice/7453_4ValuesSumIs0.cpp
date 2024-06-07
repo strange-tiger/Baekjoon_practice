5b190856e1d1054ff083f87d6587084760a2541a
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-07 18:43:39 +0900

    Fix : 7453_4ValuesSumIs0 실패
    이분탐색으로 더하면 0이 되는 수를 찾는다. 일반적인 이분탐색과 다르게 값의 이동 방향을 반대로 해야 해서 헷갈린 부분이 있다.
    숫자가 중복으로 있는 경우 세기 편하게 하기 위해 map을 활용했다.
    
    또 시간 초과가 났다. map과 같은 해시 자료구조를 아예 배제해봐야 겠다.

--- _2024_BOJ_Practice/7453_4ValuesSumIs0.cpp
+++ _2024_BOJ_Practice/7453_4ValuesSumIs0.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <set>
+#include <map>
+#include <algorithm>
 #define MAX 4000
 #define L long long
 using namespace std;
 
 int n, cnt = 0;
 int arr[4][MAX];
-L added[2][MAX * MAX];
-multiset<L> hashCheck;
+L sum[2][MAX * MAX];
+map<L, int> hashCnt;
 
 void input()
 {
@@ -23,16 +24,42 @@ void input()
 	for (int i = 0; i < 2; ++i)
 		for (int j = 0; j < n; ++j)
 			for (int k = 0; k < n; ++k)
-				added[i][j * n + k] = arr[i * 2][j] + arr[i * 2 + 1][k];
+				sum[i][j * n + k] = arr[i * 2][j] + arr[i * 2 + 1][k];
 }
 
 void solve()
 {
-	for (int i = 0; i < n * n; ++i)
-		hashCheck.insert(added[0][i]);
+	int N = n * n;
 
-	for (int i = 0; i < n * n; ++i)
-		cnt += hashCheck.count(-added[1][i]);
+	sort(sum[1], sum[1] + N);
+	for (int i = 0; i < N; ++i)
+	{
+		if (hashCnt.count(sum[1][i]) == 0)
+			hashCnt[sum[1][i]] = 1;
+		else
+			hashCnt[sum[1][i]] = hashCnt[sum[1][i]] + 1;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		int s = -1, e = N, mid;
+
+		while (s + 1 < e)
+		{
+			mid = (s + e) / 2;
+
+			if (sum[0][i] == -sum[1][mid])
+			{
+				s = mid;
+				cnt += hashCnt[sum[1][mid]];
+				break;
+			}
+			else if (sum[0][i] < -sum[1][mid])
+				s = mid;
+			else
+				e = mid;
+		}
+	}
 
 	cout << cnt;
 }
