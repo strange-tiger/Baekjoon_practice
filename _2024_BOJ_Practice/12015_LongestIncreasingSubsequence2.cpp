3933221e8cee821c0a87a0c493897635851d72ec
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-08 12:49:54 +0900

    Fix : 12015_LongestIncreasingSubsequence2 해결
    한 번 벡터로 싹 갈아보았다.
    하 이젠 또 왜 맞지?

--- _2024_BOJ_Practice/12015_LongestIncreasingSubsequence2.cpp
+++ _2024_BOJ_Practice/12015_LongestIncreasingSubsequence2.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 #define MAX 1000001
 using namespace std;
 
-int N, idx, ans = 0;
-int A[MAX];
-int X[MAX];
+int N, tmp, idx;
+vector<int> A, ans;
 
-int binarySearch(int key)
+int binarySearch(int k)
 {
-	int lo = 0, hi = ans - 1, mid;
+	int lo = 0, hi = ans.size() - 1, mid;
 
-	while (lo + 1 < hi)
+	while (lo < hi)
 	{
 		mid = (lo + hi) / 2;
 
-		if (X[mid] > key)
+		if (ans[mid] >= k)
 			hi = mid;
-		else
-			lo = mid + 1;
+		else lo = mid + 1;
 	}
 
 	return hi;
@@ -32,28 +30,28 @@ void input()
 
 	cin >> N;
 	for (int i = 0; i < N; ++i)
-		cin >> A[i];
+	{
+		cin >> tmp;
+		A.push_back(tmp);
+	}
 }
 
 void solve()
 {
-	X[1] = A[0];
-	ans = 1;
+	ans.push_back(A.front());
 
 	for (int i = 1; i < N; ++i)
 	{
-		if (A[i] > X[ans])
-		{
-			X[++ans] = A[i];
-		}
+		if (A[i] > ans.back())
+			ans.push_back(A[i]);
 		else
 		{
 			idx = binarySearch(A[i]);
-			X[idx] = A[i];
+			ans[idx] = A[i];
 		}
 	}
 
-	cout << ans;
+	cout << ans.size();
 }
 
 int main()
