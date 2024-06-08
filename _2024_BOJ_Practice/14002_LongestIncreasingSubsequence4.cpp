3c529fabbeb92d85b125bde7d3229c847f048366
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-08 13:16:55 +0900

    Feat : 14002_LongestIncreasingSubsequence4 실패
    12015 문제에 부분 수열을 따로 저장할 벡터만 넣어주었다.
    먼저 나이브하게 벡터 갱신 때마다 다시 저장하는 방식으로 해보았다.
    으음 틀렸다.

---
+++ _2024_BOJ_Practice/14002_LongestIncreasingSubsequence4.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#define MAX 1000001
+using namespace std;
+
+int N, tmp, idx;
+int A[MAX];
+vector<int> X, ans;
+
+int binarySearch(int k)
+{
+	int lo = 0, hi = X.size() - 1, mid;
+
+	while (lo < hi)
+	{
+		mid = (lo + hi) / 2;
+
+		if (X[mid] >= k)
+			hi = mid;
+		else
+			lo = mid + 1;
+	}
+
+	return hi;
+}
+
+void input()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> A[i];
+}
+
+void solve()
+{
+	X.push_back(A[0]);
+	ans.push_back(A[0]);
+
+	for (int i = 1; i < N; ++i)
+	{
+		if (A[i] > X.back())
+		{
+			X.push_back(A[i]);
+			ans.push_back(A[i]);
+		}
+		else
+		{
+			idx = binarySearch(A[i]);
+			X[idx] = A[i];
+
+			if (idx == X.size() - 1)
+			{
+				ans.clear();
+
+				for (int j = 0; j < X.size(); ++j)
+					ans.push_back(X[j]);
+			}
+		}
+	}
+
+	cout << X.size() << '\n';
+	for (int i = 0; i < ans.size(); ++i)
+		cout << ans[i] << ' ';
+}
+
+int main()
+{
+	input();
+	solve();
+	return 0;
+}
