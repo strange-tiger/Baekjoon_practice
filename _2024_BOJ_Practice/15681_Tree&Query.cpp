ac6df2e84bf0dae36b829332324f5b2b94b44234
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-09 20:24:35 +0900

    Feat : 15681_Tree&Query 진행 중
    양방향 그래프를 만들고 이를 dfs로 순회하며 트리 구조로 만들었다. 여기까진 잘 되었으나 dp 계산하는 부분에서 막혔다. 리프 노드면 dp값이 1이 되도록 하고 부모 노드에서 다 더해지게 해봤는데 수가 부족하다. 처음부터 dp 기본값을 1로 맞춰볼까 한다.

---
+++ _2024_BOJ_Practice/15681_Tree&Query.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#define MAX 100001
+using namespace std;
+
+int N, R, Q;
+int parent[MAX] = { 0 };
+int dp[MAX] = { 0 };
+bool isVisited[MAX] = { false };
+vector<int> g[MAX];
+
+void input()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> R >> Q;
+
+	int u, v;
+	for (int i = 0; i < N - 1; ++i)
+	{
+		cin >> u >> v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+}
+
+void dfs(int node)
+{
+	isVisited[node] = true;
+	int cnt = 0;
+	for (int next : g[node])
+	{
+		if (isVisited[next])
+			continue;
+		parent[next] = node;
+		dfs(next);
+		++cnt;
+	}
+
+	if (cnt == 0)
+		dp[node] = 1;
+	if (parent[node] > 0)
+		dp[parent[node]] += dp[node];
+}
+
+int childNum(int u)
+{
+	return dp[u];
+}
+
+void solve()
+{
+	parent[R] = -1;
+	dfs(R);
+
+	int u;
+	while (Q--)
+	{
+		cin >> u;
+		cout << childNum(u) << '\n';
+	}
+}
+
+int main()
+{
+	input();
+	solve();
+	return 0;
+}
