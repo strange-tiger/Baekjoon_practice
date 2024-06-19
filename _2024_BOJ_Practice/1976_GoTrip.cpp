66e880414f267c2780e4c238851edfb6777273ec
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-19 09:16:15 +0900

    Feat : 1976_GoTrip 실패
    그래프 문제이다. 하지만 각 정점이 한 그래프에 포함되어 있는가, 즉 한 그룹에 속한 요소인가를 판별하는 문제이므로 유니온 파인드로 풀어보았다.
    82% 즈음에서 틀렸다.

---
+++ _2024_BOJ_Practice/1976_GoTrip.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+bool able = true;
+int parent[201];
+
+int Find(int x)
+{
+	if (parent[x] == x)
+		return x;
+	return parent[x] = Find(parent[x]);
+}
+
+void Union(int x, int y)
+{
+	x = parent[x];
+	y = parent[y];
+
+	if (x < y)
+		parent[y] = parent[x];
+	else
+		parent[x] = parent[y];
+}
+
+void input()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cin >> M;
+
+	for (int i = 0; i < N; ++i)
+		parent[i] = i;
+
+	int connect;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			cin >> connect;
+
+			if (connect)
+				Union(i, j);
+		}
+	}
+}
+
+void solve()
+{
+	int front, back;
+
+	cin >> back;
+	for (int i = 1; i < M; ++i)
+	{
+		cin >> front;
+
+		if (Find(front) != Find(back))
+			able = false;
+
+		back = front;
+	}
+
+	if (able)
+		cout << "YES";
+	else
+		cout << "NO";
+}
+
+int main()
+{
+	input();
+	solve();
+	return 0;
+}
