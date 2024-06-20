683220ad1e38c8cca6fa161c01659a30652d32a5
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-20 09:49:29 +0900

    Fix : 6549_LargestRectangleInaHistogram 실패
    분할 정복으로 풀 수 있는 문제였다. 다만 분할 정복을 위한 재귀 함수를 떠올리지 못해 결국 다른 분의 코드를 참고했다. 오랜만에 만난 플래티넘 문제인데 부끄럽고 한심하다.
    논리는 다음과 같다.
    히스토그램을 좌우로 나누어 왼쪽의 최대와 오른쪽의 최대를 가운데 선을 포함해 계산하고, 비교한다.
    이를 양쪽 뭉치에 대해서도 반복해 쪼개고 쪼개기를 반복하는 것이다.
    각 막대를 계산하는 느낌이 아니라 각 변을 계산하는 느낌으로 하니 이해가 쉬웠다.
    
    그런데 시간초과로 틀렸다. 이런.
    
    참고 :
    https://as-j.tistory.com/98

--- _2024_BOJ_Practice/6549_LargestRectangleInaHistogram.cpp
+++ _2024_BOJ_Practice/6549_LargestRectangleInaHistogram.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <algorithm>
+#define L long long
 using namespace std;
 
 int n;
-int h[100001];
+L h[100001];
+
+L getRect(L left, L right)
+{
+	L mid = (left + right) / 2;
+	L height = h[mid];
+	L l = mid - 1, r = mid;
+	L width = 2;
+	L lh = 0, rh = 0, minh = min(h[l], h[r]);
+	L area = width * minh;
+
+	if (mid == left)
+		return h[mid];
+
+	while (l >= 0 && r < n)
+	{
+		if (l > 0)
+			lh = h[l - 1];
+		else
+			lh = 0;
+
+		if (r < n - 1)
+			rh = h[r + 1];
+		else
+			rh = 0;
+
+		minh = min(minh, max(lh, rh));
+
+		if (lh > rh)
+			--l;
+		else
+			++r;
+
+		++width;
+		area = max(area, width * minh);
+	}
+	return max(max(getRect(left, mid - 1), getRect(mid, right)), area);
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -12,10 +52,9 @@ int main()
 	while (cin >> n, n != 0)
 	{
 		for (int i = 0; i < n; ++i)
-		{
 			cin >> h[i];
-		}
+		cout << getRect(0, n) << '\n';
 	}
-}
 
-// https://www.acmicpc.net/problem/6549
+	return 0;
+}
