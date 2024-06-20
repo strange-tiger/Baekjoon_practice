183f325cc9bd71a9e761169289fac40a9a5ff85b
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-20 10:49:31 +0900

    Feat : 4195_VirtualFriends 실패
    유니온 파인드 문제이다. map으로 이름에 코드를 대응시키고 코드로 그룹을 지으며 friends 배열에 그 그룹의 크기를 저장해 문제를 해결했다.
    38%에서 실패.

---
+++ _2024_BOJ_Practice/4195_VirtualFriends.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<map>
+#include<string>
+#define MAX 200001
+using namespace std;
+
+int T, cnt, num;
+int parent[MAX];
+int friends[MAX];
+int fCode[2];
+string fName[2];
+map<string, int> name;
+
+int Find(int x)
+{
+	if (parent[x] == x)
+		return x;
+	return parent[x] = Find(parent[x]);
+}
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+
+	if (a > b)
+	{
+		parent[a] = b;
+		friends[b] += friends[a];
+	}
+	else
+	{
+		parent[b] = a;
+		friends[a] += friends[b];
+	}
+}
+
+void input()
+{
+	name.clear();
+	cnt = 0;
+
+	for (int i = 0; i < MAX; ++i)
+	{
+		parent[i] = i;
+		friends[i] = 1;
+	}
+
+	cin >> num;
+}
+
+void solve()
+{
+	for (int i = 0; i < num; ++i)
+	{
+		cin >> fName[0] >> fName[1];
+
+		for (int j = 0; j < 2; ++j)
+		{
+			if (name.find(fName[j]) == name.end())
+			{
+				name[fName[j]] = ++cnt;
+				fCode[j] = cnt;
+			}
+			else
+				fCode[j] = name[fName[j]];
+		}
+
+		Union(fCode[0], fCode[1]);
+		cout << friends[Find(fCode[0])] << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+
+	while (T--)
+	{
+		input();
+		solve();
+	}
+
+	return 0;
+}
