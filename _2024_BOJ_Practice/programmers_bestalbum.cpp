60f4a13abe8ea731e32896c3c5d24f71d6b76d83
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-21 14:57:09 +0900

    Feat : programmers_bestalbum 진행
    프로그래머스의 레벨 3 해시 문제.
    테스트와 기록을 위해 실행 파일을 만들어보았다.
    약간 헤맸지만 생각보다 간단히 풀 수 있었다. 설명이 헷갈려 삽질도 꽤 했다. 장르에 속한 노래의 갯수로 순서를 정하는 줄 알았다.
    일단 설명하자면, 해시 구조인 맵으로 장르 당 곡 수를 세고 이 곡 수와 재생 수, 고유번호를 한 묶음으로 가진 벡터를 만들어 세 조건을 비교하는 비교함수를 바탕으로 정렬했다.
    근데 다시 읽어보니 '속한 노래가 많이 재생된' 장르가 우선이었다. 고쳐야 한다.

---
+++ _2024_BOJ_Practice/programmers_bestalbum.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#define F first
+#define S second
+using namespace std;
+
+bool cmp(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b)
+{
+    if (a.first == b.first)
+    {
+        if (a.second.first == b.second.first)
+        {
+            return a.second.second < b.second.second;
+        }
+        return a.second.first > b.second.first;
+    }
+    return a.first > b.first;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    map<string, int> genreNum;
+
+    for (string g : genres)
+        genreNum[g] = 0;
+    for (string g : genres)
+        ++genreNum[g];
+
+    vector<pair<int, pair<int, int>>> album;
+
+    for (int i = 0; i < plays.size(); ++i)
+        album.push_back({ genreNum[genres[i]], { plays[i], i }});
+
+    sort(album.begin(), album.end(), cmp);
+
+    for (string g : genres)
+        genreNum[g] = 0;
+
+    int code;
+    for (int i = 0; i < album.size(); ++i)
+    {
+        code = album[i].second.second;
+        
+        if (genreNum[genres[code]] == 2)
+            continue;
+        ++genreNum[genres[code]];
+
+        answer.push_back(code);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int n, tmp;
+    string str;
+    vector<string> g;
+    vector<int> p;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> str;
+        g.push_back(str);
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> tmp;
+        p.push_back(tmp);
+    }
+
+    vector<int> ans = solution(g, p);
+
+    for (int i : ans)
+        cout << i << ' ';
+}
