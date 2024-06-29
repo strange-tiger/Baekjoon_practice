1d823f8d770e1dc353cc389203e67ff44d5954c4
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2024-06-29 16:32:15 +0900

    Feat : 1012_OrganicCabbage_DFS(recursion) 해결
    DFS 복습 - 재귀 버전
    역시 이쪽 dfs가 더 취향에 맞는다.
    stack에서 코드를 좀 덜어내니 바로 만들 수 있었다.

---
+++ _2024_BOJ_Practice/1012_OrganicCabbage_DFS(recursion).cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+int T, M = 0, N = 0, K, ans = 0;
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+int cabbage[50][50] = { 0 };
+bool isVisited[50][50] = { 0 };
+
+void dfs(int x, int y)
+{
+	isVisited[y][x] = true;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx >= 0 && nx < M && ny >= 0 && ny < N)
+		{
+			if (cabbage[ny][nx] == 1 && !isVisited[ny][nx])
+			{
+				dfs(nx, ny);
+			}
+		}
+	}
+}
+
+void init()
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			cabbage[i][j] = 0;
+			isVisited[i][j] = false;
+		}
+	}
+
+	ans = 0;
+}
+
+void input()
+{
+	init();
+
+	cin >> M >> N >> K;
+
+	int x, y;
+	for (int i = 0; i < K; ++i)
+	{
+		cin >> x >> y;
+		cabbage[y][x] = 1;
+	}
+}
+
+void solve()
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			if (cabbage[i][j] == 1 && !isVisited[i][j])
+			{
+				++ans;
+				dfs(j, i);
+			}
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+
+	while (T--)
+	{
+		input();
+		solve();
+	}
+
+	return 0;
+}
